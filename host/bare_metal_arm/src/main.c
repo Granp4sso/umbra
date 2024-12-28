5d2107e557003c85cee9e25e93eee18aa6dd9e73
Gr4npasso <stefanomercogliano@gmail.com>
2024-12-28 11:24:49 +0100

    Refactoring of umbra directories

---
+++ host/bare_metal_arm/src/main.c
@@ -0,0 +1,9 @@
+//extern const volatile uint32_t _peripheral_GPIO_out_start;
+
+int main(){
+
+
+    while(1);
+
+    return 0;
+}
---
+++ src/hardware/platform/stm32l552/old_boot/src/non_secure_callable.rs
@@ -0,0 +1,21 @@
+use core::arch::global_asm;
+
+//use crate::s_fn;
+
+#[cfg(all(target_arch = "arm", target_os = "none"))]
+extern "C" {
+    pub fn nsc_fn();
+}
+#[cfg(all(target_arch = "arm", target_os = "none"))]
+global_asm!(
+    "
+    .section .nsc, \"a\"
+    .global nsc_fn
+    .extern s_fn         
+
+    nsc_fn:
+        sg
+        bl s_fn
+
+    "
+);
